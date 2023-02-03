11774586bab44d641d42cf7d79905cdeb3e055f9
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-02-03 20:21:49 -0300

    Project created

---
+++ src/main/java/com/starking/diarista/DiaristaApplication.java
@@ -0,0 +1,13 @@
+package com.starking.diarista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaristaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaristaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starking/diarista/DiaristaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starking.diarista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaristaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
