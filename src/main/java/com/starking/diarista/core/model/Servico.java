1f9c22dd4e47a86b963a91624df2cc4282aa2f8a
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-02-05 13:17:07 -0300

    adicionando anotações na entidade

--- src/main/java/com/starking/diarista/core/model/Servico.java
+++ src/main/java/com/starking/diarista/core/model/Servico.java
@@ -5,7 +5,10 @@ import java.math.BigDecimal;
 
 import com.starking.diarista.core.model.enums.Icone;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -31,39 +34,57 @@ public class Servico implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(length = 50, nullable = false)
 	private String nome;
 	
+	@Column(name = "valor_minimo", nullable = false)
 	private BigDecimal valorMinimo;
 	
+	@Column(name = "qtd_horas", nullable = false)
 	private Integer qtdHoras;
 	
+	@Column(name = "porcentagem_Comissao", nullable = false)
 	private BigDecimal porcentagemComissao;
 	
+	@Column(name = "horas_Quarto", nullable = false)
 	private Integer horasQuarto;
 	
+	@Column(name = "valor_Quarto", nullable = false)
 	private BigDecimal valorQuarto;
 	
+	@Column(name = "horas_Sala", nullable = false)
 	private Integer horasSala;
 	
+	@Column(name = "valor_Sala", nullable = false)
 	private BigDecimal valorSala;	
 	
+	@Column(name = "horas_Banheiro", nullable = false)
 	private Integer horasBanheiro;
 	
+	@Column(name = "valor_Banheiro", nullable = false)
 	private BigDecimal valorBanheiro;	
 	
+	@Column(name = "horas_Cozinha", nullable = false)
 	private Integer horasCozinha;
 	
+	@Column(name = "valor_Cozinha", nullable = false)
 	private BigDecimal valorCozinha;	
 	
+	@Column(name = "horas_Quintal", nullable = false)
 	private Integer horasQuintal;
 	
+	@Column(name = "valor_Quintal", nullable = false)
 	private BigDecimal valorQuintal;	
 	
+	@Column(name = "horas_Outros", nullable = false)
 	private Integer horasOutros;
 	
+	@Column(name = "valor_Outros", nullable = false)
 	private BigDecimal valorOutros;
 	
+	@Enumerated(EnumType.STRING)
 	private Icone icone;
 	
+	@Column(nullable = false)
 	private Integer posicao;
 }
