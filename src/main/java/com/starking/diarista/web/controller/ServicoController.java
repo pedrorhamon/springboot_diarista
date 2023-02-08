432b7e977692b695482103d69d5012ada3ecdacc
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-02-08 20:35:46 -0300

    Utilizando camada de DTO no Controller

--- src/main/java/com/starking/diarista/web/controller/ServicoController.java
+++ src/main/java/com/starking/diarista/web/controller/ServicoController.java
@@ -9,9 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.starking.diarista.core.dtos.ServicoDTO;
 import com.starking.diarista.core.enums.Icone;
-import com.starking.diarista.core.model.Servico;
 import com.starking.diarista.core.repositories.ServicoRepository;
+import com.starking.diarista.web.mappers.WebServicoMapper;
 
 @RestController
 @RequestMapping("/admin/servicos")
@@ -19,6 +20,9 @@ public class ServicoController {
 	
 	@Autowired
 	private ServicoRepository servicoRepository;
+	
+	@Autowired
+	private WebServicoMapper servicoMapper;
 	
 	@GetMapping
 	public ModelAndView buscarTodos() {
@@ -29,23 +33,31 @@ public class ServicoController {
 	@GetMapping("/cadastrar")
 	public ModelAndView cadastrar() {
 		var modelAndView = new ModelAndView("admin/servico/form");
-		return modelAndView.addObject("servico", new Servico());
+		return modelAndView.addObject("form", new ServicoDTO());
 	}
 	
 	@PostMapping("/cadastrar")
-	public String cadastrar(Servico servico) {
+	public String cadastrar(ServicoDTO servicoDto) {
+		var servico = servicoMapper.toModel(servicoDto);
 		this.servicoRepository.save(servico);
 		return "redirect:/admin/servicos";
 	}
 	
+	@SuppressWarnings("deprecation")
 	@GetMapping("/{id}/editar")
 	public ModelAndView editar(@PathVariable Long id) {
 		var modelAndView = new ModelAndView("admin/servico/form");
-		return modelAndView.addObject("servico", this.servicoRepository.findById(id));
+		
+		var servico = this.servicoRepository.getById(id);
+		var dto = servicoMapper.toDTO(servico);
+		return modelAndView.addObject("servico", dto);
 	}
 	
 	@PostMapping("/{id}/editar")
-	public String editar(@PathVariable Long id, Servico servico) {
+	public String editar(@PathVariable Long id, ServicoDTO servicoDTO) {
+		var servico = servicoMapper.toModel(servicoDTO);
+		
+		servico.setId(id);
 		this.servicoRepository.save(servico);
 		
 		return "redirect:/admin/servicos";
