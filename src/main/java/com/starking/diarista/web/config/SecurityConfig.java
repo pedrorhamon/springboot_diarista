91ade55822f0f953d385eb6d5371fced53544354
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-02-21 18:52:33 -0300

    Atualizando a configuração do Spring Security

--- src/main/java/com/starking/diarista/web/config/SecurityConfig.java
+++ src/main/java/com/starking/diarista/web/config/SecurityConfig.java
@@ -1,18 +1,37 @@
 package com.starking.diarista.web.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.starking.diarista.core.enums.TipoUsuario;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsService detailsService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests()
+		.antMatchers("/admin/**")
+		.hasAuthority(TipoUsuario.ADMIN.toString())
+		.anyRequest().authenticated();
 	}
 
-	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(detailsService)
+		.passwordEncoder(passwordEncoder);
+	}
 }
